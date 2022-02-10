036013f8efb26397ff81ef8560ae03aedfbd5133
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-02-11 10:08:55 +1100

    updated buy button

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     // "BASE_URL": "http://localhost:3000",
     BASE_URL: "https://zenix.vercel.app",
+    //BASE_URL: "https://jz65m.sse.codesandbox.io",
     MONGODB_URL:
       "mongodb+srv://0:0@cluster0.ufs0l.mongodb.net/zenix?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "YOUR_ACCESS_TOKEN_SECRET",
@@ -9,6 +10,6 @@ module.exports = {
     PAYPAL_CLIENT_ID: "YOUR_PAYPAL_CLIENT_ID",
     CLOUD_UPDATE_PRESET: "YOUR_CLOUD_UPDATE_PRESET",
     CLOUD_NAME: "YOUR_CLOUD_NAME",
-    CLOUD_API: "YOUR_CLOUD_API",
-  },
+    CLOUD_API: "YOUR_CLOUD_API"
+  }
 };
