bd4a5b17d7c7e52dabde8335f51562c5ddea417d
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-02-17 16:31:21 +1100

    paypal and category

--- components/Filter.js
+++ components/Filter.js
@@ -1,69 +1,74 @@
-import React, {useState, useEffect} from 'react'
-import filterSearch from '../utils/filterSearch'
-import {getData} from '../utils/fetchData'
-import {useRouter} from 'next/router'
+import React, { useState, useEffect } from "react";
+import filterSearch from "../utils/filterSearch";
+import { getData } from "../utils/fetchData";
+import { useRouter } from "next/router";
 
-const Filter = ({state}) => {
-    const [search, setSearch] = useState('')
-    const [sort, setSort] = useState('')
-    const [category, setCategory] = useState('')
+const Filter = ({ state }) => {
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [category, setCategory] = useState("");
 
-    const {categories} = state
+  const { categories } = state;
 
-    const router = useRouter()
+  const router = useRouter();
 
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+    filterSearch({ router, category: e.target.value });
+  };
 
-    const handleCategory = (e) => {
-        setCategory(e.target.value)
-        filterSearch({router, category: e.target.value})
-    }
+  const handleSort = (e) => {
+    setSort(e.target.value);
+    filterSearch({ router, sort: e.target.value });
+  };
 
-    const handleSort = (e) => {
-        setSort(e.target.value)
-        filterSearch({router, sort: e.target.value})
-    }
+  useEffect(() => {
+    filterSearch({ router, search: search ? search.toLowerCase() : "all" });
+  }, [search]);
 
-    useEffect(() => {
-        filterSearch({router, search: search ? search.toLowerCase() : 'all'})
-    },[search])
+  return (
+    <div className="input-group">
+      <div className="input-group-prepend col-md-2 px-0 mt-2">
+        <select
+          className="custom-select text-capitalize"
+          value={category}
+          onChange={handleCategory}
+        >
+          <option value="all">All Products</option>
 
-    return (
-        <div className="input-group">
-            <div className="input-group-prepend col-md-2 px-0 mt-2">
-                <select className="custom-select text-capitalize"
-                value={category} onChange={handleCategory}>
+          {categories.map((item) => (
+            <option key={item._id} value={item.name}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+      </div>
 
-                    <option value="all">All Products</option>
+      <form autoComplete="off" className="mt-2 col-md-8 px-0">
+        <input
+          type="text"
+          className="form-control"
+          list="title_product"
+          value={search.toLowerCase()}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
 
-                    {
-                        categories.map(item => (
-                            <option key={item._id} value={item._id}>{item.name}</option>
-                        ))
-                    }
-                </select>
-            </div>
+      <div className="input-group-prepend col-md-2 px-0 mt-2">
+        <select
+          className="custom-select text-capitalize"
+          value={sort}
+          onChange={handleSort}
+        >
+          <option value="-createdAt">Newest</option>
+          <option value="oldest">Oldest</option>
+          <option value="-sold">Best sales</option>
+          <option value="-price">Price: Hight-Low</option>
+          <option value="price">Price: Low-Hight</option>
+        </select>
+      </div>
+    </div>
+  );
+};
 
-            <form autoComplete="off" className="mt-2 col-md-8 px-0">
-                <input type="text" className="form-control" list="title_product"
-                value={search.toLowerCase()} onChange={e => setSearch(e.target.value)} />
-            </form>
-
-            <div className="input-group-prepend col-md-2 px-0 mt-2">
-                <select className="custom-select text-capitalize"
-                value={sort} onChange={handleSort}>
-
-                     <option value="-createdAt">Newest</option>
-                     <option value="oldest">Oldest</option>
-                     <option value="-sold">Best sales</option>
-                     <option value="-price">Price: Hight-Low</option>
-                     <option value="price">Price: Low-Hight</option>
-
-                </select>
-            </div>
-
-
-        </div>
-    )
-}
-
-export default Filter
+export default Filter;
--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   env: {
     BASE_URL: "https://zenix.vercel.app",
-    //BASE_URL: "https://jz65m.sse.codesandbox.io",
+    // BASE_URL: "https://jz65m.sse.codesandbox.io",
     MONGODB_URL:
       "mongodb+srv://0:0@cluster0.ufs0l.mongodb.net/zenix?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "YOUR_ACCESS_TOKEN_SECRET",
     REFRESH_TOKEN_SECRET: "YOUR_REFRESH_TOKEN_SECRET",
-    PAYPAL_CLIENT_ID: "YOUR_PAYPAL_CLIENT_ID",
+    PAYPAL_CLIENT_ID:
+      "AX6j-AfMDTWCoF2Yxjx2hYPk40ABjkTs0RV9F6E0EPJb9qiGIKCJT2K52Dtd8KuHeAiY4h5Ohhb-coFK",
     CLOUD_UPDATE_PRESET: "YOUR_CLOUD_UPDATE_PRESET",
     CLOUD_NAME: "YOUR_CLOUD_NAME",
     CLOUD_API: "YOUR_CLOUD_API"
